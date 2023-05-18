33114097f5aea80344b14201984510787665a440
yago <yagom10@hotmail.com>
2023-05-18 00:06:21 -0700

    complete

--- main.js
+++ main.js
@@ -2,6 +2,30 @@
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
+const articleHearts = document.querySelectorAll(".like-glyph");
+
+function likeCallback(e) {
+  const heart = e.target;
+  mimicServerCall("bogusUrl")
+  .then(function(){
+    if( heart.innerText === EMPTY_HEART) {
+      heart.innerText = FULL_HEART;
+      heart.className ="";
+    }
+  })
+.catch(function(error) {
+const modal = document.getElementById("modal");
+modal.className = "";
+modal.innerText = error;
+setTimeout( () => modal.className = "hidden", 3000);
+ });
+} 
+
+for(const glyph of articleHearts) {
+  glyph.addEventListener("click", likeCallback);
+}
+
+
 // Your JavaScript code goes here!
 
 
